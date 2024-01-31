b950b27a5eca3ee4d53e703d4a8134cbd0d080cb
BANIKIM <72877425+BANIKIM@users.noreply.github.com>
2024-01-31 13:46:33 +0900

    [Feat] 프로젝트 기본세팅
    VR 기본 기능 그랩 피치 구현

---
+++ Assets/3.Script/Hand/AnimateHandOnInput.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class AnimateHandOnInput : MonoBehaviour
+{
+
+    public InputActionProperty pinchanim;
+    public InputActionProperty gripanim;
+    public Animator handanim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float triggerValue = pinchanim.action.ReadValue<float>();
+        handanim.SetFloat("Trigger", triggerValue);
+
+        float gripValue = gripanim.action.ReadValue<float>();
+        handanim.SetFloat("Grip", gripValue);
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Camping_Cook_PJ
+ VR 캠핑 요리게임
