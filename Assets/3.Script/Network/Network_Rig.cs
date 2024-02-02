2a4bc05f6730a29b4789a720d8086c0bb249350e
BANIKIM <72877425+BANIKIM@users.noreply.github.com>
2024-02-02 09:38:07 +0900

    [Feat] VR Network test

--- Assets/3.Script/Network/Network_Rig.cs
+++ Assets/3.Script/Network/Network_Rig.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Mirror;
 
-public class Network_Rig : MonoBehaviour
+public class Network_Rig : NetworkBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public Hardware_Rig hardware_rig;
+
+    public Network_Hand leftNetworkHand;
+    public Network_Hand rightNetworkHand;
+    public Network_Head networkHead;
+
+    private void Start()
     {
-        
+        if(isLocalPlayer)
+        {
+            hardware_rig = GameObject.FindWithTag("Player").GetComponent<Hardware_Rig>();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
