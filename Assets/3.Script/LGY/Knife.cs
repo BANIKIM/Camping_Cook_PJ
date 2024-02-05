aac33b96f319fb0089f421f433c56663752990f0
Limkwan1004 <janyouns@gmail.com>
2024-02-05 11:02:19 +0900

    [Feat] Skewer State

--- Assets/3.Script/LGY/Knife.cs
+++ Assets/3.Script/LGY/Knife.cs
@@ -4,17 +4,18 @@ using UnityEngine;
 
 public class Knife : MonoBehaviour
 {
-    Vector3 presiousPos; // 이전 위치
-    LayerMask food_layer = 6;
+    private Vector3 presiousPos; // 이전 위치
+    private LayerMask food_layer = 6;
 
+    [SerializeField] float cutting_speed = 100f;
 
-    private void Update()
+    private void FixedUpdate()
     {
-
-        RaycastHit hit; // target
+        RaycastHit hit; 
         if (Physics.Raycast(transform.position, transform.forward, out hit, 5, food_layer))
         {
-            if (Vector3.Angle(transform.position - presiousPos, hit.transform.up) >= 130f) // position으로 방향 벡터를 구하고, 방향벡터와 cube의 transfrom.up 각도를 비교
+            // position으로 방향 벡터를 구하고, 방향벡터와 cube의 transfrom.up 각도를 비교
+            if (Vector3.Angle(transform.position - presiousPos, hit.transform.up) >= cutting_speed) 
             {
                 Ingredient ingredient = hit.transform.GetComponent<Ingredient>();
                 ingredient.current_slice_count++;
