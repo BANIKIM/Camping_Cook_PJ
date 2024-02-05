31c82f29c6021b883440b617fd93caa18bc9d237
HurDoyun <148195154+HurDoyun@users.noreply.github.com>
2024-02-05 18:05:36 +0900

    Grilled_meat 2

--- Assets/3.Script/HDY/GrilledSystem.cs
+++ Assets/3.Script/HDY/GrilledSystem.cs
@@ -7,5 +7,11 @@ public class GrilledSystem : MonoBehaviour
     public Material[] mat = new Material[3];
 
     //https://cagongman.tistory.com/64
-    //private int grilled
+    private int grilled_stat = 0;
+
+    public void ChangeMat()
+    {
+        grilled_stat = ++grilled_stat
+    }
+
 }
