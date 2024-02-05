03a1e4c5699235df10eed831ccbe20d6b8e84e21
Limkwan1004 <janyouns@gmail.com>
2024-02-05 13:12:50 +0900

    [Feat] SliceObj & SwingObj
    디버깅 해봐야함

--- Assets/3.Script/LGY/Swing_Obj.cs
+++ Assets/3.Script/LGY/Swing_Obj.cs
@@ -2,23 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Knife : MonoBehaviour
+public class Swing_Obj : MonoBehaviour
 {
     private Vector3 presiousPos; // 이전 위치
-    private LayerMask food_layer = 6;
+    [SerializeField] private LayerMask interaction_layer;
 
     [SerializeField] float cutting_speed = 100f;
 
     private void FixedUpdate()
     {
         RaycastHit hit; 
-        if (Physics.Raycast(transform.position, transform.forward, out hit, 5, food_layer))
+        if (Physics.Raycast(transform.position, transform.forward, out hit, 5, interaction_layer))
         {
             // position으로 방향 벡터를 구하고, 방향벡터와 cube의 transfrom.up 각도를 비교
             if (Vector3.Angle(transform.position - presiousPos, hit.transform.up) >= cutting_speed) 
             {
-                Ingredient ingredient = hit.transform.GetComponent<Ingredient>();
-                ingredient.current_slice_count++;
+                Slice_Obj slice_obj = hit.transform.GetComponent<Slice_Obj>();
+                slice_obj.current_slice_count++;
             }
         }
 
