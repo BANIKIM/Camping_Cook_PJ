6c326abc50745501fab97bf911c1f7cac95e1302
HurDoyun <148195154+HurDoyun@users.noreply.github.com>
2024-02-05 17:37:26 +0900

    GrilledSys start

---
+++ Assets/3.Script/HDY/GrilledSystem.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrilledSystem : MonoBehaviour
+{
+    public Material[] mat = new Material[3];
+
+    //https://cagongman.tistory.com/64
+    //private int grilled
+}
