47542fac55e6390c324623e94ba5187e572d0382
BANIKIM <72877425+BANIKIM@users.noreply.github.com>
2024-02-06 13:52:45 +0900

    [Fix] 싱크바 훅으로 코드수정

--- Assets/3.Script/Hand/NetWork_Hand_grab.cs
+++ Assets/3.Script/Hand/NetWork_Hand_grab.cs
@@ -7,9 +7,9 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class NetWork_Hand_grab : NetworkBehaviour
 {
     private XRGrabInteractable grabInteractable;
-    [SyncVar]
+    [SyncVar(hook = nameof(OnObjectPositionChanged))]
     public Vector3 objPos;
-    [SyncVar]
+    [SyncVar(hook = nameof(OnObjectRotationChanged))]
     public Quaternion objRot;
 
     private void Start()
@@ -41,7 +41,19 @@ public class NetWork_Hand_grab : NetworkBehaviour
     void RpcUpdateObjectPosition(Vector3 position, Quaternion rotation)
     {
         // 클라이언트에서만 실행됨
-        transform.position = position;
-        transform.rotation = rotation;
+        objPos = position;
+        objRot = rotation;
+    }
+
+    // objPos 값이 변경될 때 호출되는 메서드
+    void OnObjectPositionChanged(Vector3 oldPos, Vector3 newPos)
+    {
+        transform.position = newPos;
+    }
+
+    // objRot 값이 변경될 때 호출되는 메서드
+    void OnObjectRotationChanged(Quaternion oldRot, Quaternion newRot)
+    {
+        transform.rotation = newRot;
     }
 }
