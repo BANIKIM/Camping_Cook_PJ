429dd6fff55435917792750ac862f48c84c58ef3
Limkwan1004 <janyouns@gmail.com>
2024-02-08 17:05:37 +0900

    [Feat] EzySlice
    디테일 깎아야함

--- Assets/3.Script/LGY/Slice_Obj2.cs
+++ Assets/3.Script/LGY/Slice_Obj2.cs
@@ -2,31 +2,61 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using EzySlice;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class Slice_Obj2 : MonoBehaviour
 {
-    public Transform knifedebug;
-    public GameObject target;
+    public LayerMask sliceable_layer;
+    private Vector3 presiousPos; // 이전 위치
+    public Transform Pos;
+    public Material cross;
+    private float cutting_speed = 0f;
+    private float cut_force = 100f;
 
-    private void Update()
+    private bool Test = false;
+
+
+    public void FixedUpdate()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+
+        Debug.DrawRay(transform.position, Vector3.down, Color.red, 5);
+        if (Physics.Raycast(Pos.position, Pos.forward, out RaycastHit hit, 0.05f, sliceable_layer))
         {
-            EzySlice_Obj(target);
+            if (Test.Equals(false)) SliceObj(hit.transform.gameObject);
+            
+        }
+        presiousPos = transform.position;
+    }
+
+    public void SliceObj(GameObject target)
+    {
+        Test = true;
+        Vector3 slice_normal = Vector3.Cross(transform.position - presiousPos, transform.forward);
+        SlicedHull hull = target.Slice(target.transform.position, slice_normal);
+
+        if (hull != null)
+        {
+            GameObject upper_hull = hull.CreateUpperHull(target, cross);
+            SetupSlicedComponent(upper_hull);
+            GameObject lower_hull = hull.CreateLowerHull(target, cross);
+            SetupSlicedComponent(lower_hull);
+
+            Destroy(target);
+            Test = false;
         }
     }
 
-    public void EzySlice_Obj(GameObject target)
+
+    public void SetupSlicedComponent(GameObject slice_obj)
     {
-        SlicedHull hull = target.Slice(knifedebug.position, knifedebug.up);
+        Rigidbody rigid = slice_obj.AddComponent<Rigidbody>();
+        MeshCollider col = slice_obj.AddComponent<MeshCollider>();
+        XRGrabInteractable xrgrab = slice_obj.AddComponent<XRGrabInteractable>();
+        col.convex = true;
+        rigid.AddExplosionForce(cut_force, transform.position, 1);
 
-        if(hull !=null)
-        {
-            GameObject upper_hull = hull.CreateUpperHull(target);
-            GameObject lower_hull = hull.CreateLowerHull(target);
+        slice_obj.gameObject.layer = 6;
 
-            Destroy(target);
-        }
 
     }
 
