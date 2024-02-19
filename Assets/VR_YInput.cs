018a09ec057cab33af2ee5fbffe7fe62828a9739
0Baek <148294815+0Baek@users.noreply.github.com>
2024-02-19 18:12:53 +0900

    [Feat] 태블릿 ui 위치 구현

---
+++ Assets/VR_YInput.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.InputSystem;
+using System.Collections;
+
+public class VR_YInput : MonoBehaviour
+{
+    public InputActionReference HandCanvas;
+    public GameObject Canvas1;
+    public GameObject Canvas2;
+
+    private bool isCanvasActive = false;
+    private bool isButtonPressed = false;
+
+    private void OnEnable()
+    {
+        HandCanvas.action.started += OnButtonPress;
+        HandCanvas.action.canceled += OnButtonRelease;
+    }
+
+    private void OnDisable()
+    {
+        HandCanvas.action.started -= OnButtonPress;
+        HandCanvas.action.canceled -= OnButtonRelease;
+    }
+
+    private IEnumerator CheckLongPress()
+    {
+        yield return new WaitForSeconds(2f);
+
+        if (isButtonPressed)
+        {
+            // 2초 이상 버튼이 눌려 있는 경우
+            isCanvasActive = false;
+            Canvas1.SetActive(isCanvasActive);
+            Canvas2.SetActive(true);
+        }
+    }
+
+    private void OnButtonPress(InputAction.CallbackContext context)
+    {
+        isButtonPressed = true;
+        isCanvasActive = !isCanvasActive;
+        Canvas1.SetActive(isCanvasActive);
+        StartCoroutine(CheckLongPress());
+    }
+
+    private void OnButtonRelease(InputAction.CallbackContext context)
+    {
+        isButtonPressed = false;
+        StopAllCoroutines();
+    }
+}
