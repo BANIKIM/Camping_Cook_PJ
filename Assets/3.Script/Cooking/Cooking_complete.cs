56783e3cfc990821e74b3ad55cade66c69913e8f
Limkwan1004 <janyouns@gmail.com>
2024-02-21 09:17:26 +0900

    [Fix] Cooking_Complete

--- Assets/3.Script/Cooking/Cooking_complete.cs
+++ Assets/3.Script/Cooking/Cooking_complete.cs
@@ -9,15 +9,15 @@ public class Cooking_complete : MonoBehaviour
     {
         if(collision.gameObject.layer ==6)
         {
-
+            Ingredient ingred = collision.gameObject.GetComponent<Ingredient>();
         }
     }
     //이름
 
     //자르기
-    public int isCut(Collision collision)
+    public int isCut(Ingredient ingred)
     {
-        if(collision.gameObject.GetComponent<Ingredient>().isSlice)
+        if(ingred.isSlice)
         {
             return 10000;
         }
@@ -25,20 +25,26 @@ public class Cooking_complete : MonoBehaviour
     }
 
     //소금 후추
-    public int isSeasoning(Collision collision)
+    public int isSeasoning(Ingredient ingred)
     {
-        if(true)
+
+        if(ingred.seasoning_ingred.pepper_s.Equals(Seasoning_Ingredient.Pepper_S.Peppered))
         {
             //소금 후추
-            return 1100;
+            return 1000;
         }
+        if(ingred.seasoning_ingred.salt_s.Equals(Seasoning_Ingredient.Salt_S.Salted))
+        {
+            return 100;
+        }
+
         return 0;
         
     }
     //꽂기
-    public int isSkewer(Collision collision)
+    public int isSkewer(Ingredient ingred)
     {
-        if(true)
+        if(ingred.skewer_ingred.skewer_state.Equals(Skewer_State.Inserted))
         {
             return 10;
         }
@@ -46,9 +52,9 @@ public class Cooking_complete : MonoBehaviour
     }
 
     //익힘
-    public int isCooked(Collision collision)
+    public int isCooked(Ingredient ingred)
     {
-        if(collision.gameObject.GetComponent<Cooked_Ingredient>().cooked_state == Cooked_Ingredient.Cooked_State.Cook)
+        if(ingred.cooked_ingred.cooked_state == Cooked_Ingredient.Cooked_State.Cook)
         {
             return 1;
         }
