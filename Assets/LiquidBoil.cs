c587df08a66e73e108e1899cbe7ddfb09254bd25
BANIKIM <72877425+BANIKIM@users.noreply.github.com>
2024-02-21 15:30:15 +0900

    [Feat] Boil 다시제작

--- Assets/LiquidBoil.cs
+++ Assets/LiquidBoil.cs
@@ -8,9 +8,15 @@ public class LiquidBoil : MonoBehaviour
     public Tool_heat heat;
     public GameObject smoke;
     public float HP = 10;
+    public GameObject[] Foods;
+    private Cooked_Ingredient cooked;
+    private Ingredient ingredient;
+
     void Start()
     {
         heat = Cook_Tool_pot.gameObject.GetComponent<Tool_heat>();
+        cooked = GetComponent<Cooked_Ingredient>();
+        ingredient = GetComponent<Ingredient>();
     }
 
     private void FixedUpdate()
@@ -19,6 +25,7 @@ public class LiquidBoil : MonoBehaviour
         {
             HP -= Time.deltaTime;
             Onsmoke();//스모크 이펙트 키기
+            isTan();//시간지나면 탐
         }
     }
 
@@ -26,8 +33,14 @@ public class LiquidBoil : MonoBehaviour
     {
         if (other.gameObject.layer == 6)
         {
-            Boil boil = other.GetComponent<Boil>();
-            HP += boil.HP;
+            HP = 5;//바로 타는거 방지
+            HP += 5;
+            for (int i = 0; i < Foods.Length; i++)
+            {
+                Foods[i].SetActive(true);
+            }
+            
+            Destroy(other.gameObject);
         }
     }
 
@@ -36,10 +49,27 @@ public class LiquidBoil : MonoBehaviour
         if (HP < 5)
         {
             smoke.SetActive(true);
+            if(Foods[0].activeSelf)
+            {
+                cooked.Change_Skewer_State(Cooked_Ingredient.Cooked_State.Cook);//스테이터스 변경
+                ingredient.Cook_ch_mat();//머테리얼 변경
+                ingredient._crossMat = ingredient._mesh.material;
+            }         
         }
         else
         {
             smoke.SetActive(false);
+        }
+    }
+
+    private void isTan()
+    {
+        if(HP<0 && Foods[0].activeSelf)
+        {
+            cooked.Change_Skewer_State(Cooked_Ingredient.Cooked_State.Burned);//스테이터스 변경
+            ingredient.Cook_ch_mat();//머테리얼 변경
+            ingredient._crossMat = ingredient._mesh.material;
+
         }
     }
 }
