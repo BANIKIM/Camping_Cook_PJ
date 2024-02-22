2a89380da9797c129c220892b6b0baed3f465051
Limkwan1004 <janyouns@gmail.com>
2024-02-22 11:46:11 +0900

    [Fix] LiquidBoil

--- Assets/LiquidBoil.cs
+++ Assets/LiquidBoil.cs
@@ -7,12 +7,14 @@ public class LiquidBoil : MonoBehaviour
     public GameObject Cook_Tool_pot;
     public Tool_heat heat;
     public GameObject smoke;
+
     public float HP = 10;
-    public GameObject[] Foods;
+    public Ingredient[] Foods;
     private Cooked_Ingredient cooked;
+
     private Ingredient ingredient;
     private int number = 0;
-    private bool a;
+    public List<int> _ingred_Type_L = new List<int>();
 
     void Start()
     {
@@ -37,28 +39,20 @@ public class LiquidBoil : MonoBehaviour
 
         if (other.gameObject.layer == 6) //조건에서 받아야 하는 Ingredient_Type 타입을 받아야 함 이거 0을 어떻게 받냐?
         {
-            HP = 10;//바로 타는거 방지
             HP += 5;
+            Ingredient_Type type = other.GetComponent<Ingredient>()._ingredient_Type;
+
             Destroy(other.gameObject);
    
             for (int i = 0; i < Foods.Length; i++)
             {
-                a = false;
-                if (Foods[i].GetComponent<Ingredient>()._ingredient_Type == other.GetComponent<Ingredient>()._ingredient_Type)
+                if (!Foods[i].enabled && !Foods[i]._ingredient_Type.Equals(type))
                 {
-                    a = true;
+                    _ingred_Type_L.Add((int)type);
+                    Foods[i].transform.gameObject.SetActive(true);
                     break;
                 }
             }
-            if(!a)
-            {
-                Foods[number].GetComponent<Ingredient>()._ingredient_Type = other.GetComponent<Ingredient>()._ingredient_Type;
-                Foods[number].SetActive(true);
-                number += 1;
-            }
-          
-
-
         }
     }
 
@@ -67,7 +61,7 @@ public class LiquidBoil : MonoBehaviour
         if (HP < 5)
         {
             smoke.SetActive(true);
-            if (Foods[0].activeSelf)
+            if (Foods[0].transform.gameObject.activeSelf)
             {
                 cooked.Change_Skewer_State(Cooked_Ingredient.Cooked_State.Cook);//스테이터스 변경
                 ingredient.Cook_ch_mat();//머테리얼 변경
@@ -82,7 +76,7 @@ public class LiquidBoil : MonoBehaviour
 
     private void isTan()
     {
-        if (HP < 0 && Foods[0].activeSelf)
+        if (HP < 0 && Foods[0].transform.gameObject.activeSelf)
         {
             cooked.Change_Skewer_State(Cooked_Ingredient.Cooked_State.Burned);//스테이터스 변경
             ingredient.Cook_ch_mat();//머테리얼 변경
