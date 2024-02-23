1051bc8e82cd9d17a37f67a4de28e0d9be7447c5
Limkwan1004 <janyouns@gmail.com>
2024-02-23 16:02:58 +0900

    [Fix] RewardSystem

--- Assets/3.Script/LGY/RecipeSetting.cs
+++ Assets/3.Script/LGY/RecipeSetting.cs
@@ -14,7 +14,8 @@ public enum Cooking_Type
 public class RecipeSetting : MonoBehaviour
 {
     // Dictionary (요리 / 레시피)
-    public Dictionary<int, List<int>> _recipe_D = new Dictionary<int, List<int>>();
+    public Dictionary<int, List<int>> _recipePrep_D = new Dictionary<int, List<int>>();
+    public Dictionary<int, List<int>> _recipeCook_D = new Dictionary<int, List<int>>();
 
     public void SetDefaultRecipe()
     {
@@ -28,36 +29,46 @@ public class RecipeSetting : MonoBehaviour
     private void MashMellow_R()
     {
         // 마쉬멜로우 구이
-        List<int> Temp = new List<int> { 200011 };
-        _recipe_D.Add((int)Cooking_Type.Mashmellow, Temp);
+        List<int> perptemp = new List<int> { 20001 };
+        List<int> cooktemp = new List<int> { 21 };
+        _recipePrep_D.Add((int)Cooking_Type.Mashmellow, perptemp);
+        _recipeCook_D.Add((int)Cooking_Type.Mashmellow, cooktemp);
     }
 
     private void Beef_Stew_R()
     {
         // 비프스튜
-        List<int> Temp = new List<int> {111101, 410001, 510001 };
-        _recipe_D.Add((int)Cooking_Type.Beef_Stew, Temp);
+        List<int> perptemp = new List<int> { 11110, 41000, 51000 };
+        List<int> cooktemp = new List<int> { 11, 41, 51 };
+        _recipePrep_D.Add((int)Cooking_Type.Beef_Stew, perptemp);
+        _recipeCook_D.Add((int)Cooking_Type.Beef_Stew, cooktemp);
     }
 
     private void Skewers_Flatter_R()
     {
         // 꼬치플레터
-        List<int> Temp = new List<int> { 111011, 610011, 810011 };
-        _recipe_D.Add((int)Cooking_Type.Skewers_Flatter, Temp);
+        List<int> perptemp = new List<int> { 11110, 41000, 51000 };
+        List<int> cooktemp = new List<int> { 11, 61, 81 };
+        _recipePrep_D.Add((int)Cooking_Type.Skewers_Flatter, perptemp);
+        _recipeCook_D.Add((int)Cooking_Type.Skewers_Flatter, cooktemp);
     }
 
     private void Steak_R()
     {
         // 스테이크
-        List<int> Temp = new List<int> { 101101, 610001 };
-        _recipe_D.Add((int)Cooking_Type.Steak, Temp);
+        List<int> perptemp = new List<int> { 11110, 41000, 51000 };
+        List<int> cooktemp = new List<int> { 11, 61 };
+        _recipePrep_D.Add((int)Cooking_Type.Steak, perptemp);
+        _recipeCook_D.Add((int)Cooking_Type.Steak, cooktemp);
     }
 
     private void Grilled_Salmon_R()
     {
         // 연어구이
-        List<int> Temp = new List<int> { 311101, 710001 };
-        _recipe_D.Add((int)Cooking_Type.Grilled_Salmon, Temp);
+        List<int> perptemp = new List<int> { 11110, 41000, 51000 };
+        List<int> cooktemp = new List<int> { 31, 71 };
+        _recipePrep_D.Add((int)Cooking_Type.Grilled_Salmon, perptemp);
+        _recipeCook_D.Add((int)Cooking_Type.Grilled_Salmon, cooktemp);
     }
 
 
